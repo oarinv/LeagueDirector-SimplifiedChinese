d8164f214afba633e305e50d17ec553830e46266
oarinv <heizory@yandex.com>
2025-02-09 20:17:46 +0800

    Fix detection of League of Legends game clients

--- build/build.bat
+++ build/build.bat
@@ -1,7 +1,7 @@
 @echo off
 cd %~dp0
 pip install pipenv==2022.3.24
-pipenv install --skip-lock
+pipenv install 
 pipenv run pip install pyinstaller==6.6.0
 pipenv run pyinstaller build.spec --noconfirm --workpath=out/build --distpath=out/dist
 ISCC.exe install.iss
