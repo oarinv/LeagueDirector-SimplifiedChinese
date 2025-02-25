f6dff29e53ac96e59307204bef23f59b0dd39d77
oarinv <heizory@yandex.com>
2025-02-25 19:34:07 +0800

    use uv

--- build/build.sh
+++ build/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd "${0%/*}"
-pip install pipenv
-pipenv install --skip-lock
-pipenv run pip install pyinstaller==6.6.0
-pipenv run pyinstaller build.spec --windowed --noconfirm --workpath=out/build --distpath=out/dist
+uv venv .venv
+uv pip install
+uv pip install pyinstaller==6.6.0
+.venv/bin/pyinstaller build.spec --windowed --noconfirm --workpath=out/build --distpath=out/dist
--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 @echo off
-pip install pipenv==2022.3.24
-pipenv install --skip-lock
-pipenv run python -u -m leaguedirector.app
+uv venv .venv
+uv pip install
+uv run -m leaguedirector.app
